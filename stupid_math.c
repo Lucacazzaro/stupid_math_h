d99d316647ac2cb9073a6f0ce04908a498749961
Luca Cazzaro <lucacazzaro1@gmail.com>
2024-02-29 12:40:01 +0100

    Annullo l?ultimo commit

--- stupid_math.c
+++ stupid_math.c
@@ -1,4 +1,19 @@
 #include "stupid_math.h"
-float mean(int a,int b) {
-return (a + b) / 2;
+
+float mean(int a, int b)  {
+        return (float) (a + b) /(float) 2;
 }
+
+complex_float_t complex_mean(complex_int_t a, complex_int_t b){
+        complex_float_t res = {0, 0};
+
+        res.re = (a.re + b.re) /2;      //res.re = mean(a.re, b.re);
+        res.im = (a.im + b.im) /2;      //res.im = mean(a.im, b.im);
+
+        return res;
+}
+
+
+
+
+
