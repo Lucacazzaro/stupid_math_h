4671c54e32b328e30c42b996352ee06efec465a1
Luca Cazzaro <lucacazzaro1@gmail.com>
2024-02-29 19:40:18 +0100

    correzzione collisioni merge

--- stupid_math.c
+++ stupid_math.c
@@ -5,12 +5,3 @@ float mean(int a, int b)  {
         return (float) (a + b) /(float) 2;
 }
 
-complex_float_t complex_mean(complex_int_t a, complex_int_t b){
-        complex_float_t res = {0, 0};
-
-        res.re = (a.re + b.re) /2;      //res.re = mean(a.re, b.re);
-        res.im = (a.im + b.im) /2;      //res.im = mean(a.im, b.im);
-
-        return res;
-}
-
